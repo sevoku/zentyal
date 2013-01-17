744e525fc64dcb47677deddc9359d3c180393a38
Javier Amor García <jamor@zentyal.com>
2013-01-17 15:15:37 +0100

    fixed use sentence

--- main/mailfilter/src/EBox/MailFilter/Model/AntispamACL.pm
+++ main/mailfilter/src/EBox/MailFilter/Model/AntispamACL.pm
@@ -23,7 +23,7 @@ use EBox;
 
 use EBox::Exceptions::Internal;
 use EBox::Gettext;
-use EBox::Types::Boolean;
+use EBox::Types::Select;
 use EBox::MailFilter::Types::AmavisSender;
 
 # Group: Public methods
